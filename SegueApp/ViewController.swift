667d38a576e04ed4f3d0041e60da5cb542d159f6
busraaguler <gulerbusra1998@gmail.com>
2022-05-20 04:51:43 -0700

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by busra guler on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
